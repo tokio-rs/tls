b040a9a65f1b16569652dd2b7564363055d7682e
quininer <quininer@live.com>
2018-08-16 20:44:37 +0800

    Test use lazy_static!

--- tests/test.rs
+++ tests/test.rs
@@ -1,81 +1,89 @@
+#[macro_use] extern crate lazy_static;
 extern crate rustls;
 extern crate tokio;
 extern crate tokio_rustls;
 extern crate webpki;
 
-#[cfg(feature = "unstable-futures")] extern crate futures;
-
 use std::{ io, thread };
 use std::io::{ BufReader, Cursor };
 use std::sync::Arc;
 use std::sync::mpsc::channel;
-use std::net::{ SocketAddr, IpAddr, Ipv4Addr };
+use std::net::SocketAddr;
 use tokio::net::{ TcpListener, TcpStream };
-use rustls::{ Certificate, PrivateKey, ServerConfig, ClientConfig };
+use rustls::{ ServerConfig, ClientConfig };
 use rustls::internal::pemfile::{ certs, rsa_private_keys };
 use tokio_rustls::{ ClientConfigExt, ServerConfigExt };
 
 const CERT: &str = include_str!("end.cert");
 const CHAIN: &str = include_str!("end.chain");
 const RSA: &str = include_str!("end.rsa");
-const HELLO_WORLD: &[u8] = b"Hello world!";
 
+lazy_static!{
+    static ref TEST_SERVER: (SocketAddr, &'static str, &'static str) = {
+        use tokio::prelude::*;
+        use tokio::io as aio;
 
-fn start_server(cert: Vec<Certificate>, rsa: PrivateKey) -> SocketAddr {
-    use tokio::prelude::*;
-    use tokio::io as aio;
+        let cert = certs(&mut BufReader::new(Cursor::new(CERT))).unwrap();
+        let mut keys = rsa_private_keys(&mut BufReader::new(Cursor::new(RSA))).unwrap();
 
-    let mut config = ServerConfig::new(rustls::NoClientAuth::new());
-    config.set_single_cert(cert, rsa)
-        .expect("invalid key or certificate");
-    let config = Arc::new(config);
+        let mut config = ServerConfig::new(rustls::NoClientAuth::new());
+        config.set_single_cert(cert, keys.pop().unwrap())
+            .expect("invalid key or certificate");
+        let config = Arc::new(config);
 
-    let (send, recv) = channel();
+        let (send, recv) = channel();
 
-    thread::spawn(move || {
-        let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 0);
-        let listener = TcpListener::bind(&addr).unwrap();
+        thread::spawn(move || {
+            let addr = SocketAddr::from(([127, 0, 0, 1], 0));
+            let listener = TcpListener::bind(&addr).unwrap();
 
-        send.send(listener.local_addr().unwrap()).unwrap();
+            send.send(listener.local_addr().unwrap()).unwrap();
 
-        let done = listener.incoming()
-            .for_each(move |stream| {
-                let done = config.accept_async(stream)
-                    .and_then(|stream| aio::read_exact(stream, vec![0; HELLO_WORLD.len()]))
-                    .and_then(|(stream, buf)| {
-                        assert_eq!(buf, HELLO_WORLD);
-                        aio::write_all(stream, HELLO_WORLD)
-                    })
-                    .then(|_| Ok(()));
+            let done = listener.incoming()
+                .for_each(move |stream| {
+                    let done = config.accept_async(stream)
+                        .and_then(|stream| {
+                            let (reader, writer) = stream.split();
+                            aio::copy(reader, writer)
+                        })
+                        .then(|_| Ok(()));
 
-                tokio::spawn(done);
-                Ok(())
-            })
-            .map_err(|err| panic!("{:?}", err));
+                    tokio::spawn(done);
+                    Ok(())
+                })
+                .map_err(|err| panic!("{:?}", err));
 
-        tokio::run(done);
-    });
+            tokio::run(done);
+        });
 
-    recv.recv().unwrap()
+        let addr = recv.recv().unwrap();
+        (addr, "localhost", CHAIN)
+    };
 }
 
-fn start_client(addr: &SocketAddr, domain: &str, chain: Option<BufReader<Cursor<&str>>>) -> io::Result<()> {
+
+fn start_server() -> &'static (SocketAddr, &'static str, &'static str) {
+    &*TEST_SERVER
+}
+
+fn start_client(addr: &SocketAddr, domain: &str, chain: &str) -> io::Result<()> {
     use tokio::prelude::*;
     use tokio::io as aio;
+
+    const FILE: &'static [u8] = include_bytes!("../README.md");
 
     let domain = webpki::DNSNameRef::try_from_ascii_str(domain).unwrap();
     let mut config = ClientConfig::new();
-    if let Some(mut chain) = chain {
-        config.root_store.add_pem_file(&mut chain).unwrap();
-    }
+    let mut chain = BufReader::new(Cursor::new(chain));
+    config.root_store.add_pem_file(&mut chain).unwrap();
     let config = Arc::new(config);
 
     let done = TcpStream::connect(addr)
         .and_then(|stream| config.connect_async(domain, stream))
-        .and_then(|stream| aio::write_all(stream, HELLO_WORLD))
-        .and_then(|(stream, _)| aio::read_exact(stream, vec![0; HELLO_WORLD.len()]))
+        .and_then(|stream| aio::write_all(stream, FILE))
+        .and_then(|(stream, _)| aio::read_exact(stream, vec![0; FILE.len()]))
         .and_then(|(stream, buf)| {
-            assert_eq!(buf, HELLO_WORLD);
+            assert_eq!(buf, FILE);
             aio::shutdown(stream)
         })
         .map(drop);
@@ -86,22 +94,15 @@ fn start_client(addr: &SocketAddr, domain: &str, chain: Option<BufReader<Cursor<
 
 #[test]
 fn pass() {
-    let cert = certs(&mut BufReader::new(Cursor::new(CERT))).unwrap();
-    let mut keys = rsa_private_keys(&mut BufReader::new(Cursor::new(RSA))).unwrap();
-    let chain = BufReader::new(Cursor::new(CHAIN));
+    let (addr, domain, chain) = start_server();
 
-    let addr = start_server(cert, keys.pop().unwrap());
-    start_client(&addr, "localhost", Some(chain)).unwrap();
+    start_client(addr, domain, chain).unwrap();
 }
 
-#[should_panic]
 #[test]
 fn fail() {
-    let cert = certs(&mut BufReader::new(Cursor::new(CERT))).unwrap();
-    let mut keys = rsa_private_keys(&mut BufReader::new(Cursor::new(RSA))).unwrap();
-    let chain = BufReader::new(Cursor::new(CHAIN));
+    let (addr, domain, chain) = start_server();
 
-    let addr = start_server(cert, keys.pop().unwrap());
-
-    start_client(&addr, "google.com", Some(chain)).unwrap();
+    assert_ne!(domain, &"google.com");
+    assert!(start_client(addr, "google.com", chain).is_err());
 }
