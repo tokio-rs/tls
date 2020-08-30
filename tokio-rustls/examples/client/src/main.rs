cff714ceefcc030056ed7247adfecc8ae7432b35
quininer <quininer@live.com>
2020-08-30 23:11:08 +0800

    tokio-rustls: release 0.14.1
    * Support half-closed states #23
    * Update examples

--- tokio-rustls/examples/client/src/main.rs
+++ tokio-rustls/examples/client/src/main.rs
@@ -1,88 +1,80 @@
-use std::io;
+use argh::FromArgs;
 use std::fs::File;
+use std::io;
+use std::io::BufReader;
+use std::net::ToSocketAddrs;
 use std::path::PathBuf;
 use std::sync::Arc;
-use std::net::ToSocketAddrs;
-use std::io::BufReader;
-use futures_util::future;
-use structopt::StructOpt;
-use tokio::runtime;
+use tokio::io::{copy, split, stdin as tokio_stdin, stdout as tokio_stdout, AsyncWriteExt};
 use tokio::net::TcpStream;
-use tokio::io::{
-    AsyncWriteExt,
-    copy, split,
-    stdin as tokio_stdin, stdout as tokio_stdout
-};
-use tokio_rustls::{ TlsConnector, rustls::ClientConfig, webpki::DNSNameRef };
+use tokio_rustls::{rustls::ClientConfig, webpki::DNSNameRef, TlsConnector};
 
-
-#[derive(StructOpt)]
+/// Tokio Rustls client example
+#[derive(FromArgs)]
 struct Options {
+    /// host
+    #[argh(positional)]
     host: String,
 
     /// port
-    #[structopt(short="p", long="port", default_value="443")]
+    #[argh(option, short = 'p', default = "443")]
     port: u16,
 
     /// domain
-    #[structopt(short="d", long="domain")]
+    #[argh(option, short = 'd')]
     domain: Option<String>,
 
     /// cafile
-    #[structopt(short="c", long="cafile", parse(from_os_str))]
-    cafile: Option<PathBuf>
+    #[argh(option, short = 'c')]
+    cafile: Option<PathBuf>,
 }
 
-
-fn main() -> io::Result<()> {
-    let options = Options::from_args();
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let options: Options = argh::from_env();
 
     let addr = (options.host.as_str(), options.port)
         .to_socket_addrs()?
         .next()
         .ok_or_else(|| io::Error::from(io::ErrorKind::NotFound))?;
     let domain = options.domain.unwrap_or(options.host);
-    let content = format!(
-        "GET / HTTP/1.0\r\nHost: {}\r\n\r\n",
-        domain
-    );
+    let content = format!("GET / HTTP/1.0\r\nHost: {}\r\n\r\n", domain);
 
-    let mut runtime = runtime::Builder::new()
-        .basic_scheduler()
-        .enable_io()
-        .build()?;
     let mut config = ClientConfig::new();
     if let Some(cafile) = &options.cafile {
         let mut pem = BufReader::new(File::open(cafile)?);
-        config.root_store.add_pem_file(&mut pem)
+        config
+            .root_store
+            .add_pem_file(&mut pem)
             .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))?;
     } else {
-        config.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
+        config
+            .root_store
+            .add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
     }
     let connector = TlsConnector::from(Arc::new(config));
 
-    let fut = async {
-        let stream = TcpStream::connect(&addr).await?;
+    let stream = TcpStream::connect(&addr).await?;
 
-        let (mut stdin, mut stdout) = (tokio_stdin(), tokio_stdout());
+    let (mut stdin, mut stdout) = (tokio_stdin(), tokio_stdout());
 
-        let domain = DNSNameRef::try_from_ascii_str(&domain)
-            .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid dnsname"))?;
+    let domain = DNSNameRef::try_from_ascii_str(&domain)
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid dnsname"))?;
 
-        let mut stream = connector.connect(domain, stream).await?;
-        stream.write_all(content.as_bytes()).await?;
+    let mut stream = connector.connect(domain, stream).await?;
+    stream.write_all(content.as_bytes()).await?;
 
-        let (mut reader, mut writer) = split(stream);
-        future::select(
-            copy(&mut reader, &mut stdout),
-            copy(&mut stdin, &mut writer)
-        )
-            .await
-            .factor_first()
-            .0?;
+    let (mut reader, mut writer) = split(stream);
 
-        Ok(())
-    };
+    tokio::select! {
+        ret = copy(&mut reader, &mut stdout) => {
+            ret?;
+        },
+        ret = copy(&mut stdin, &mut writer) => {
+            ret?;
+            writer.shutdown().await?
+        }
+    }
 
-    runtime.block_on(fut)
+    Ok(())
 }
