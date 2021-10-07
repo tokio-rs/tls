7ba08ee55b67d00da92a49696ddedd064f560068
quininer <quininer@live.com>
2021-10-07 11:14:16 +0800

    don't throw eof error to keep consistency

--- tokio-rustls/src/common/mod.rs
+++ tokio-rustls/src/common/mod.rs
@@ -62,7 +62,6 @@ pub struct Stream<'a, IO, C> {
     pub io: &'a mut IO,
     pub session: &'a mut C,
     pub eof: bool,
-    pub unexpected_eof: bool,
 }
 
 impl<'a, IO: AsyncRead + AsyncWrite + Unpin, C, SD> Stream<'a, IO, C>
@@ -77,7 +76,6 @@ where
             // The state so far is only used to detect EOF, so either Stream
             // or EarlyData state should both be all right.
             eof: false,
-            unexpected_eof: false,
         }
     }
 
@@ -288,12 +286,7 @@ where
 
                 Err(err) if err.kind() == io::ErrorKind::UnexpectedEof => {
                     self.eof = true;
-                    self.unexpected_eof = true;
-                    if prev == buf.remaining() {
-                        Poll::Ready(Err(err))
-                    } else {
-                        break;
-                    }
+                    break;
                 }
 
                 // This should be unreachable.
