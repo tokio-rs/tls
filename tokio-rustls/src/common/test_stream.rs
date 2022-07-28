c2d1fe6813015e84449779d97c61e1924fc7b8c0
Dirkjan Ochtman <dirkjan@ochtman.nl>
2022-07-28 12:51:38 +0200

    Refresh test certificates for tokio-rustls (#113)
    Used the script in tokio-native-tls/scripts to generate new certs.

--- tokio-rustls/src/common/test_stream.rs
+++ tokio-rustls/src/common/test_stream.rs
@@ -286,7 +286,7 @@ fn make_pair() -> (ServerConnection, ClientConnection) {
     let (sconfig, cconfig) = utils::make_configs();
     let server = ServerConnection::new(sconfig).unwrap();
 
-    let domain = rustls::ServerName::try_from("localhost").unwrap();
+    let domain = rustls::ServerName::try_from("foobar.com").unwrap();
     let client = ClientConnection::new(cconfig, domain).unwrap();
 
     (server, client)
